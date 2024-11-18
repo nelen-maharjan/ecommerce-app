1d6f1d2dbc79ad97bc8d2e05d5481a86f99b71f0
Nelen Maharjan <neleen11@gmail.com>
2024-11-18 13:19:01 +0545

    newsletter

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -7,16 +7,16 @@ import { Button } from './ui/button';
 
 const Newsletter = () => {
   return (
-    <div className='my-8 flex gap-6'>
+    <div className='my-8 flex gap-6 p-2'>
         {/* left */}
-        <div>
+        <div className='hidden md:block'>
             <Image src={'/wholesale-electronics.jpeg'} width={550} height={400} alt='electronics' />
         </div>
         {/* right */}
         <div className='flex flex-col'>
             <h2 className='text-xl font-bold uppercase mb-3'>Subscribe to our newsletter</h2>
-            <p className='font-medium text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi molestias quas voluptatem porro minus velit nesciunt sit.</p>
-            <div className='grid grid-cols-[2fr_1fr] mt-10'>
+            <p className='font-medium text-xs md:text-sm'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam modi molestias quas voluptatem porro minus velit nesciunt sit.</p>
+            <div className='grid grid-cols-[2fr_1fr] mt-4 md:mt-10'>
                 <FormInput
                     type={'email'}
                     placeholder={'Enter your email address'}
